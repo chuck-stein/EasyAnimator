51e67a09c96087d5b37a19d343d1b7f08b58fe73
Chuck Stein <cfgstein@gmail.com>
2018-10-27 15:04:04 -0400

    finished model interface

--- src/EasyAnimator/model/IEasyAnimatorModel.java
+++ src/EasyAnimator/model/IEasyAnimatorModel.java
@@ -10,36 +10,52 @@ import java.awt.geom.Point2D;
 public interface IEasyAnimatorModel {
 
   /**
-   * Adds a Shape with the given characteristics to the model.
-   * @param type the {@link ShapeType}
-   * @param name the name of the Shape
-   * @param color the starting {@link Color} of the Shape
+   * Adds a Shape with the given characteristics to the animation.
+   *
+   * @param type     the {@link ShapeType}
+   * @param name     the name of the Shape
+   * @param color    the starting {@link Color} of the Shape
    * @param position the starting {@link Point2D position} of the Shape
-   * @param w the starting width of the Shape
-   * @param h the starting height of the Shape
+   * @param w        the starting width of the Shape
+   * @param h        the starting height of the Shape
    * @throws IllegalArgumentException if any of the given Shape characteristics are invalid:
-   * <p>-shape name already exists</p><p>-null color</p><p>-null position</p><p>-negative
-   * dimensions</p>
+   *                                  <p>-shape name already exists</p><p>-null color</p><p>-null
+   *                                  position</p><p>-negative dimensions</p>
    */
   void createShape(ShapeType type, String name, Color color, Point2D position, int w, int h)
           throws IllegalArgumentException;
 
   /**
    * Creates a State for a Shape to end up at after the given period of time.
+   *
    * @param shapeName the name of the Shape
-   * @param dt the change in time since the Shape's last State
-   * @param color the updated {@link Color} of the Shape
-   * @param position the updated {@link Point2D position} of the Shape
-   * @param w the updated width of the Shape
-   * @param h the updated height of the Shape
+   * @param dt        the change in time since the Shape's last State
+   * @param color     the updated {@link Color} of the Shape
+   * @param position  the updated {@link Point2D position} of the Shape
+   * @param w         the updated width of the Shape
+   * @param h         the updated height of the Shape
    * @throws IllegalArgumentException if any of the given state characteristics are invalid:
-   * <p>-negative delta time</p><p>-null color</p><p>-null position</p><p>-negative dimensions</p>
+   *                                  <p>-negative delta time</p><p>-null color</p><p>-null
+   *                                  position</p><p>-negative dimensions</p>
    */
   void createState(String shapeName, int dt, Color color, Point2D position, int w, int h)
           throws IllegalArgumentException;
 
+  /**
+   * Creates a textual representation of all motions from one State to the next for each Shape in
+   * the animation.
+   *
+   * @return a String listing all motions from one State to the next for each Shape in the
+   * animation.
+   */
   String getAllMotions();
 
+  /**
+   * Creates a textual representation of all Shape motions occurring at the given time.
+   *
+   * @param t the time in ticks at which motions should be returned
+   * @return a String listing all Shape motions occurring at the given time.
+   */
   String getCurrentMotions(int t);
 
 }
