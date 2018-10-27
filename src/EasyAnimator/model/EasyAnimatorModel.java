fd1c90933383a9bbc87d76c557e84c830aed6893
Chuck Stein <cfgstein@gmail.com>
2018-10-27 14:44:48 -0400

    added model interface javadocs

--- src/EasyAnimator/model/EasyAnimatorModel.java
+++ src/EasyAnimator/model/EasyAnimatorModel.java
@@ -3,7 +3,7 @@ package EasyAnimator.model;
 import java.awt.Color;
 import java.awt.geom.Point2D;
 
-public class EasyAnimatorModel implements IAnimatorModel {
+public class EasyAnimatorModel implements IEasyAnimatorModel {
 
   @Override
   public void createShape(ShapeType type, String name, Color color, Point2D position, int w, int h)
