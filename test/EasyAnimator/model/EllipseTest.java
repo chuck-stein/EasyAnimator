5e81c995107d60b2cd623ccd6f5c3c354a865ae0
tgreizer <greizer.t@husky.neu.edu>
2018-10-29 16:25:58 -0400

    New Changes

--- test/EasyAnimator/model/EllipseTest.java
+++ test/EasyAnimator/model/EllipseTest.java
@@ -7,11 +7,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class EllipseTest {
-  Shape e = new Ellipse("E", 1,Color.BLUE, new Position2D(3, 2), 2, 2);
+  Shape e;
 
   @Before
   public void setUp() {
-    Shape e = new Ellipse("E",1, Color.BLUE, new Position2D(3, 2), 2, 2);
+     e = new Ellipse("E",1, Color.BLUE, new Position2D(3, 2), 4, 2);
   }
 
   @Test
@@ -93,29 +93,29 @@ public class EllipseTest {
   public void getAllMotionsAndAddStatePars() {
     assertEquals("", e.getAllMotions());
     e.addStatePars("-move 0 0 -deltaT 7 -changeSize 0.5 0.5 -changeColor 0 0 0");
-    assertEquals("motion E   1 3 2 2 2 0 0 255    8 3 2 1 1 0 0 0", e.getAllMotions());
+    assertEquals("motion E   1 3 2 4 2 0 0 255    8 3 2 2 1 0 0 0", e.getAllMotions());
     e.addStatePars("-move 1 1 -deltaT 2");
-    assertEquals("motion E   1 3 2 2 2 0 0 255    8 3 2 1 1 0 0 0" +
+    assertEquals("motion E   1 3 2 4 2 0 0 255    8 3 2 2 1 0 0 0" +
         "\n" +
-        "motion E   8 3 2 1 1 0 0 0    10 4 3 1 1 0 0 0", e.getAllMotions());
+        "motion E   8 3 2 2 1 0 0 0    10 4 3 2 1 0 0 0", e.getAllMotions());
   }
 
   @Test
   public void getAllMotionsAndAddState() {
     assertEquals("", e.getAllMotions());
     e.addState(Color.BLACK, new Position2D(3, 2), 1, 1, 7);
-    assertEquals("motion E   1 3 2 2 2 0 0 255    8 3 2 1 1 0 0 0", e.getAllMotions());
+    assertEquals("motion E   1 3 2 4 2 0 0 255    8 3 2 1 1 0 0 0", e.getAllMotions());
   }
 
   @Test
   public void getCurrentMotion() {
     assertEquals("", e.getAllMotions());
     e.addStatePars("-move 0 0 -deltaT 7 -changeSize 0.5 0.5 -changeColor 0 0 0");
-    assertEquals("motion E   1 3 2 2 2 0 0 255    8 3 2 1 1 0 0 0", e.getCurrentMotion(2));
+    assertEquals("motion E   1 3 2 4 2 0 0 255    8 3 2 2 1 0 0 0", e.getCurrentMotion(2));
     assertEquals("", e.getCurrentMotion(9));
     e.addStatePars("-move 1 1 -deltaT 2");
-    assertEquals("motion E   1 3 2 2 2 0 0 255    8 3 2 1 1 0 0 0", e.getCurrentMotion(2));
-    assertEquals("motion E   8 3 2 1 1 0 0 0    10 4 3 1 1 0 0 0", e.getCurrentMotion(9));
+    assertEquals("motion E   1 3 2 4 2 0 0 255    8 3 2 2 1 0 0 0", e.getCurrentMotion(2));
+    assertEquals("motion E   8 3 2 2 1 0 0 0    10 4 3 2 1 0 0 0", e.getCurrentMotion(9));
   }
 
 }
