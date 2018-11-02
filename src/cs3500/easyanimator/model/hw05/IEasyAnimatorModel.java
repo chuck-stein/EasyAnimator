5cb744a1fe61aeef70a60df55ecb643447f317ba
tgreizer <greizer.t@husky.neu.edu>
2018-11-02 11:58:46 -0400

    New Changes

--- src/cs3500/easyanimator/model/hw05/IEasyAnimatorModel.java
+++ src/cs3500/easyanimator/model/hw05/IEasyAnimatorModel.java
@@ -99,4 +99,36 @@ public interface IEasyAnimatorModel {
    */
   String getCurrentMotions(int t);
 
+  /**
+   *
+   * @param type
+   * @param shapeName
+   */
+  void createShape(ShapeType type, String shapeName);
+
+  /**
+   * Adds a motion to the given shape.
+   *
+   * @param name The name of the shape this motion is added to
+   * @param t1   The start time of this transformation
+   * @param x1   The initial x-position of the shape
+   * @param y1   The initial y-position of the shape
+   * @param w1   The initial width of the shape
+   * @param h1   The initial height of the shape
+   * @param r1   The initial red color-value of the shape
+   * @param g1   The initial green color-value of the shape
+   * @param b1   The initial blue color-value of the shape
+   * @param t2   The end time of this transformation
+   * @param x2   The final x-position of the shape
+   * @param y2   The final y-position of the shape
+   * @param w2   The final width of the shape
+   * @param h2   The final height of the shape
+   * @param r2   The final red color-value of the shape
+   * @param g2   The final green color-value of the shape
+   * @param b2   The final blue color-value of the shape
+
+   */
+  void addMotion(String name,
+      int t1, int x1, int y1, int w1, int h1, int r1, int g1, int b1,
+      int t2, int x2, int y2, int w2, int h2, int r2, int g2, int b2);
 }
--- src/cs3500/easyanimator/model/hw05/Shape.java
+++ src/cs3500/easyanimator/model/hw05/Shape.java
@@ -204,6 +204,13 @@ abstract class Shape {
    */
   protected abstract String getShapeType();
 
+
+  protected void addMotion(int t1, int x1, int y1, int w1, int h1, int r1, int g1, int b1,
+      int t2, int x2, int y2, int w2, int h2, int r2, int g2, int b2){
+
+  }
+
+
   /**
    * Represents a builder for a state.
    */
@@ -278,6 +285,9 @@ abstract class Shape {
       this.tick += deltaT;
     }
   }
+
+
+
 
 }
 
