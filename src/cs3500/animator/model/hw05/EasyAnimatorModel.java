6189513f851b78097ea64b6397291004e007fe0e
tgreizer <greizer.t@husky.neu.edu>
2018-11-04 13:53:39 -0500

    New Changes

--- src/cs3500/animator/model/hw05/EasyAnimatorModel.java
+++ src/cs3500/animator/model/hw05/EasyAnimatorModel.java
@@ -12,38 +12,7 @@ import java.util.Objects;
  */
 public final class EasyAnimatorModel implements IEasyAnimatorModel {
 
-  public static final class Builder implements AnimationBuilder<IEasyAnimatorModel> {
 
-    @Override
-    public IEasyAnimatorModel build() {
-      return null;
-    }
-
-    @Override
-    public AnimationBuilder<IEasyAnimatorModel> setBounds(int x, int y, int width, int height) {
-      return null;
-    }
-
-    @Override
-    public AnimationBuilder<IEasyAnimatorModel> declareShape(String name, String type) {
-      return null;
-    }
-
-    @Override
-    public AnimationBuilder<IEasyAnimatorModel> addMotion(String name, int t1, int x1, int y1,
-        int w1, int h1, int r1, int g1, int b1, int t2, int x2, int y2, int w2, int h2, int r2,
-        int g2, int b2) {
-      return null;
-    }
-
-    @Override
-    public AnimationBuilder<IEasyAnimatorModel> addKeyframe(String name, int t, int x, int y, int w,
-        int h, int r, int g, int b) {
-      return null;
-    }
-
-
-  }
   private final List<Shape> shapes;
 
   /**
