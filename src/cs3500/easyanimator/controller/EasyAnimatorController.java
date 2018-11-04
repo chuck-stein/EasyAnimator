10cf12b7b42ff4322279bb608276e5d1080e74a6
tgreizer <greizer.t@husky.neu.edu>
2018-11-04 12:32:31 -0500

    New Changes

---
+++ src/cs3500/easyanimator/controller/EasyAnimatorController.java
@@ -0,0 +1,22 @@
+package cs3500.easyanimator.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class EasyAnimatorController implements IEasyAnimatorController, ActionListener {
+
+  @Override
+  public String processCommand(String command) {
+    return null;
+  }
+
+  @Override
+  public void go() {
+
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+
+  }
+}
